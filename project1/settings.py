54c4d577b730cdef3a37d370c4b09c476ca43201
s4-hub <syafii.newbie@gmail.com>
2019-11-27 02:02:00 -0500

    update

--- project1/settings.py
+++ project1/settings.py
@@ -127,3 +127,12 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+
+CELERY_BROKER_URL = 'amqp://localhost'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'kacab.langsa@bpjsketenagakerjaan.go.id'
+EMAIL_HOST_PASSWORD = 'Langsaa02'
